377c2e0b1c32d869f0f1269dac7216ac31092ee4
N.Sedenkov <nsedenkov@yandex.ru>
2016-08-19 16:04:33 +0300

    Develop CoordListTools

--- tools.py
+++ tools.py
@@ -175,7 +175,7 @@ class CoordListTools:
     returns: one tuple or None in case of error
     static method !
     '''
-    def getUltimateDirPoint(crdlst, direction):
+    def getUltimateDirPoint(clist, direction):
         dicDirection = {
             'N':{'elem':1, 'sign': 'more'},
             'S':{'elem':1, 'sign': 'less'},
@@ -184,8 +184,8 @@ class CoordListTools:
             }
         if direction not in dicDirection:
             return None
-        rxy = crdlst[0]
-        for txy in crdlst:
+        rxy = clist[0]
+        for txy in clist:
             if dicDirection[direction]['sign'] == 'more':
                 if txy[dicDirection[direction]['elem']] > rxy[dicDirection[direction]['elem']]:
                     rxy = txy
@@ -195,8 +195,154 @@ class CoordListTools:
         return rxy
 
     getUltimateDirPoint = staticmethod(getUltimateDirPoint)
+    
+    '''
+    input: list of tuples (x, y)
+           direction: N, S, E, W
+    returns: swapped list with start point is ultimate point
+    static method !
+    '''
+    def swapPointList(clist, direction):
+        res = []
+        SP = CoordListTools.getUltimateDirPoint(clist, direction)
+        if SP is None:
+            return None
+        res.extend(clist[clist.index(SP):len(clist)])
+        res.extend(clist[0:clist.index(SP)])
+        return res
 
+    swapPointList = staticmethod(swapPointList)
+    
+    '''
+    input: list of tuples (x, y), or any other list
+    returns: reversed list
+    static method !
+    '''
+    def reverseList(clist):
+        lIdx = 0
+        hIdx = len(clist) - 1
+        while hIdx - lIdx >= 1:
+            clist[lIdx], clist[hIdx] = clist[hIdx], clist[lIdx]
+            lIdx += 1
+            hIdx -= 1
+        return clist
+
+    reverseList = staticmethod(reverseList)
+    
+    '''
+    input: list of lists [[(x,y), (x,y), (x,y)],[(x,y), (x,y), (x,y)]]
+    returns: list of unique points
+    static method !
+    '''
+    def getUniquePointsList(listList):
+        res = []
+        for lst in listList:
+            for txy in lst:
+                if res.count(txy) == 0:
+                    res.append(txy) 
+        return res
+
+    getUniquePointsList = staticmethod(getUniquePointsList)
+    
+    '''
+    input: TWO list of lists [[(x,y), (x,y), (x,y)],[(x,y), (x,y), (x,y)]]
+    returns: list of indexes in FIRST list wich haves common points with
+             contours in SECOND list
+    static method !
+    '''
+    def findRelatedContours(lst1, lst2):
+        res = []
+        for tmpcrd in lst2:
+            idx = -1
+            m1 = set(tmpcrd)
+            for crdlst in lst1:
+                m2 = set(crdlst)
+                # if power of intersection gt 0 - nearest contour found
+                if len(m1 & m2) > 0:
+                    idx = lst1.index(crdlst)
+                    if not idx in res:
+                        res.append(idx)
+        return res
+
+    findRelatedContours = staticmethod(findRelatedContours)
+    
+    '''
+    input: TWO list of lists [[(x,y), (x,y), (x,y)],[(x,y), (x,y), (x,y)]]
+    returns: index in FIRST list wich haves point, nearest to
+             contours in SECOND list
+    static method !
+    '''
+    def findNearestContour(lst1, lst2):
+        res = -1
+        minlen = 1000000
+        dirs = ['N', 'S', 'W', 'E']
+        PP = None
+        lxy = CoordListTools.getUniquePointsList(lst1)
+
+        for tmpcrd in lst2:
+            for txy in tmpcrd:
+                for curDir in dirs:
+                    CP = CoordListTools.getUltimateDirPoint(lxy, curDir)
+                    curlen = GeometryTools.length(txy, CP)
+                    if curlen < minlen:
+                        minlen = curlen
+                        PP = CP
+
+        for crdlst in lst1:
+            if PP in crdlst:
+                res = lst1.index(crdlst)
+
+        return res
+
+    findNearestContour = staticmethod(findNearestContour)
+    
+    '''
+    input: list of lists [[(x,y), (x,y), (x,y)],[(x,y), (x,y), (x,y)]]
+    returns: list of lists sorted from direction (N, S, E, W)
+    static method !
+    '''
+    def sortPointList(listList, direction):
+        res = []
+        
+        lxy = CoordListTools.getUniquePointsList(listList)
+        uPoint = CoordListTools.getUltimateDirPoint(lxy, direction)
+        
+        # First contour - owner of ultimate point    
+        idx = -1
+        for lst in listList:
+            if uPoint in lst:
+                idx = listList.index(lst)
+        res.append(CoordListTools.swapPointList(listList.pop(idx), direction))
+        
+        looplst = []
+        looplst.extend(res)
+        while len(listList) > 0:
+            # list for founded indexes
+            lidx = CoordListTools.findRelatedContours(listList, looplst)
+            
+            # list of found contours - lives 1 iteration
+            fndlst = []
+            
+            if len(lidx) > 0:
+                i = 0
+                for idx in lidx:
+                    idx -= i # Сдвиг индекса в случае удаления нескольких записей (на 2 шаге -1 и т.д.)
+                    i += 1
+                    fndlst.append(CoordListTools.swapPointList(listList.pop(idx), direction))
+            else:
+                idx = CoordListTools.findNearestContour(listList, looplst)
+                fndlst.append(CoordListTools.swapPointList(listList.pop(idx), direction))
+
+            del looplst[0:len(looplst)]
+            looplst.extend(fndlst)
+            del fndlst[0:len(fndlst)]
+            res.extend(looplst)
+        return res
+
+    sortPointList = staticmethod(sortPointList)
 
 if __name__ == "__main__":
-    lst = [(1,1), (2,1), (3,3), (4,2)]
-    print CoordListTools.getUltimateDirPoint(lst, "A")
+    lst = [(1,1), (2,1), (3,3), (4,2), (7, 10), (3,3)]
+    print CoordListTools.swapPointList(lst, "N")
+    print CoordListTools.reverseList(lst)
+    print CoordListTools.getUniquePointsList([lst,])
